f844282758ded1c348806821d3fc309143cc63f7
Felipe Vaz <felipevaz@live.com>
2024-03-06 21:36:04 -0300

    remoçao do projeto web

--- Pokedex.Web/src/app/components/shared/helpers/utils.ts
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export function formatarData(data: string): string {
-    return moment(data).format('DD/MM/YYYY HH:mm:ss');
-}
--- Pokedex.Web/src/app/interfaces/Pagination.ts
+++
@@ -1,7 +0,0 @@
-export interface Pagination {
-    data: any[];
-    page: number;
-    pageSize: number;
-    totalItems: number;
-    totalPages: number;
-}
